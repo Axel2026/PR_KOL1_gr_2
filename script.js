b552ec1aadf7c5e178e632e9e97d428668508d36
Patryk Nytko <pnytko20@gmail.com>
2021-04-09 12:10:52 +0200

    kolokwium

---
+++ script.js
@@ -0,0 +1,7 @@
+let element = document.querySelector('.box');
+let b = document.querySelector('.button');
+
+b.addEventListener('click', function() {
+    element.style.backgroundColor = "yellow";
+});
+
---
+++ script2.js
@@ -0,0 +1,5 @@
+let bu = document.querySelector('.button2');
+
+bu.addEventListener('click', function() {
+    document.body.style.backgroundColor = "red";
+});
